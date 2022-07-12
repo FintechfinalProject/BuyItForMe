fe485d7ef67958f7887a4f35901c5962e78e67e4
jth9339 <erzangthehua@gmail.com>
2022-07-12 15:22:52 +0900

    DOM 활용 HTML 객체 저장

--- backend/public/javascripts/login.js
+++ backend/public/javascripts/login.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log('로그인 페이지')
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login)//login은 함수-> 함수 설정 해야함
+
+function login(){
+    const req = {
+        id: id.value,
+        password : password.value,
+    };
+
+    console.log(req);
+};
+
+console.log(id);
